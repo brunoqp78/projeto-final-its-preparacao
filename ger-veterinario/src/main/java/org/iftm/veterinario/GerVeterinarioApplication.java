973ef9f18d1189e68f80e647afef863b36dde2fb
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2022-10-27 16:32:33 -0300

    Etapa 1

---
+++ ger-veterinario/src/main/java/org/iftm/veterinario/GerVeterinarioApplication.java
@@ -0,0 +1,13 @@
+package org.iftm.veterinario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerVeterinarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerVeterinarioApplication.class, args);
+	}
+
+}
---
+++ ger-veterinario/src/test/java/org/iftm/veterinario/GerVeterinarioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iftm.veterinario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerVeterinarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
